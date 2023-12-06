88e2bf695504fa12eadcc8593affb312f7a6382f
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2023-12-06 16:31:57 +0800

    ADD: Untested scene file loading functions

--- GDENG03 Engine/GDENG03 Engine/SceneWriter.h
+++ GDENG03 Engine/GDENG03 Engine/SceneWriter.h
@@ -3,14 +3,22 @@
 #include "AGameObject.h"
 #include "Vector3D.h"
 #include "GameObjectManager.h"
+#include "PhysicsComponent.h"
+#include "StringUtilities.h"
+#include <string>
+#include <vector>
 #include <iostream>
 #include <fstream>
 
 class SceneWriter
 {
 public:
+	static void WriteToFile(std::string inputFilePath);
+	static void LoadFromFile(std::string inputFilePath);
+
+	//std::string directory = ""; idk what sir was cooking with this so I'm cooking my own
+
+private:
 	SceneWriter();
 	~SceneWriter();
-	void WriteToFile();
-	std::string directory = "";
 };
