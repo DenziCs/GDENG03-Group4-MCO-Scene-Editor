bbb914516d1d2b12794605a2a6ed09639abd7b62
Sir Sevenn <jewelmacuha@gmail.com>
2023-12-07 12:57:31 +0800

    Edited Project properties for imfilebrowser to work

--- GDENG03 Engine/GDENG03 Engine/SceneLoadPanel.h
+++ GDENG03 Engine/GDENG03 Engine/SceneLoadPanel.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "AUIPanel.h"
 #include "SceneWriter.h"
-#include <imfilebrowser.h>
+#include <imgui.h>
+//#include <imfilebrowser.h>
+#include "imgui/imfilebrowser.h"
 
 class SceneLoadPanel : public AUIPanel
 {
